a747fbd0a8dc156f9e25447675044369cc861786
ecokayiza <ecokayiza@gmail.com>
2025-08-29 15:46:28 +0800

    fix mobile display

--- lib/strapi.ts
+++ lib/strapi.ts
@@ -1,14 +1,19 @@
 import qs from "qs";
 
-const STRAPI_URL = process.env.STRAPI_URL || "http://localhost:1337";
+const STRAPI_URL = process.env.STRAPI_URL || "http://admin.ecokayizasweb.xyz";
 
 export async function fetchFromStrapi(path: string, params: any = {}) {
   const queryString = qs.stringify(params, { encodeValuesOnly: true });
-  const response = await fetch(`${STRAPI_URL}/api/${path}?${queryString}`);
-  await new Promise(resolve => setTimeout(resolve, 100)); // Add a short delay
-  const data = await response.json();
-  if (data.data) {
-    return data;
+  try {
+    const response = await fetch(`${STRAPI_URL}/api/${path}?${queryString}`);
+    await new Promise(resolve => setTimeout(resolve, 100)); // Add a short delay
+    const data = await response.json();
+    if (data.data) {
+      return data;
+    }
+    return { data: data };
+  } catch (err) {
+    console.error('fetchFromStrapi error:', err);
+    return null;
   }
-  return { data: data };
 }
